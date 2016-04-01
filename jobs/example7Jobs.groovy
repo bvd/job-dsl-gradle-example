687701727a241da452af29a17e5ec0d6cca36a2a
Bertus van Dalen <ek.vandalen@gmail.com>
2016-04-01 23:42:20 +0200

    Update example7Jobs.groovy

--- jobs/example7Jobs.groovy
+++ jobs/example7Jobs.groovy
@@ -1,3 +1,5 @@
+println 'example 7'
+/*
 import com.dslexample.GradleCiJobBuilder
 import com.dslexample.GrailsCiJobBuilder
 
@@ -28,3 +30,4 @@ new GradleCiJobBuilder(
     ownerAndProject: 'myorg/project3',
     tasks: 'clean test'
 ).build(this)
+*/
