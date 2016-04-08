97053d970fafd30f2081f0433a29b83a5022942f
Bertus van Dalen <bda20320@BA92-2Q5CPCG8WP.local>
2016-04-08 17:08:52 +0200

    maybe

--- jobs/trxJobs.groovy
+++ jobs/trxJobs.groovy
@@ -1,10 +1,9 @@
-//import com.dslexample.XamarinJobBuilder
+import com.dslexample.XamarinJobBuilder
 
 println '>>>>> executing trxJobs.groovy'
 
-/*
+
 new XamarinJobBuilder(
         gitUrl: 'https://nsr-it-repos.dev.infosupport.net/scm/git/NS_CCMTRX',
         projectName: "Trx"
 ).build(this)
-*/
